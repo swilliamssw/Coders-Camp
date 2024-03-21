0e4435de785668766bc5e47b698a584904767111
swilliamssw <swilliams.sb@live.com>
2024-03-21 08:43:04 +0000

    home page changes and about us changes

--- script.js
+++ script.js
@@ -34,3 +34,27 @@ const navSlide = () => {
 }
 
 window.onload = () => navSlide();
+
+
+
+
+//Scroll to top 
+
+let mybutton = document.getElementById("top-arrow");
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "fixed";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
